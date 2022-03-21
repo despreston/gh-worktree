444d889333da6d97be828fc5d7c08a035dcedaac
Des Preston <despreston@gmail.com>
2022-03-21 17:56:23 -0400

    make rest client in worktree

--- internal/worktree/worktree.go
+++ internal/worktree/worktree.go
@@ -3,12 +3,17 @@ package worktree
 import (
 	"fmt"
 
-	ghapi "github.com/cli/go-gh/pkg/api"
+	"github.com/cli/go-gh"
 	"github.com/despreston/gh-worktree/internal/worktree/commands/pr"
 	"github.com/spf13/cobra"
 )
 
-func New(restClient ghapi.RESTClient) *cobra.Command {
+func New() (*cobra.Command, error) {
+	rest, err := gh.RESTClient(nil)
+	if err != nil {
+		return nil, err
+	}
+
 	var rootCmd = &cobra.Command{
 		Use:   "worktree",
 		Short: "Git worktrees, dawg",
@@ -18,6 +23,6 @@ func New(restClient ghapi.RESTClient) *cobra.Command {
 		},
 	}
 
-	rootCmd.AddCommand(pr.New(restClient))
-	return rootCmd
+	rootCmd.AddCommand(pr.New(rest))
+	return rootCmd, nil
 }
--- main.go
+++ main.go
@@ -4,18 +4,16 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/cli/go-gh"
 	"github.com/despreston/gh-worktree/internal/worktree"
 )
 
 func main() {
-	rest, err := gh.RESTClient(nil)
+	cmd, err := worktree.New()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	cmd := worktree.New(rest)
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
